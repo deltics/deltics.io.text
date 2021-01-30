a14761a00a8e89d37ca7986f148396ee4a71d19d
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-01-30 14:54:39 +1300

    chore: Tidy up unit order

--- tests/test.dpr
+++ tests/test.dpr
@@ -7,15 +7,15 @@
 
 uses
   FastMM4,
-  Deltics.Smoketest,
-  Test.Utf8Reader in 'Test.Utf8Reader.pas',
-  Test.UnicodeReader in 'Test.UnicodeReader.pas',
+  Deltics.Smoketest
   Deltics.IO.Text.Interfaces in '..\src\Deltics.IO.Text.Interfaces.pas',
   Deltics.IO.Text in '..\src\Deltics.IO.Text.pas',
   Deltics.IO.Text.TextReader in '..\src\Deltics.IO.Text.TextReader.pas',
   Deltics.IO.Text.Types in '..\src\Deltics.IO.Text.Types.pas',
   Deltics.IO.Text.Unicode in '..\src\Deltics.IO.Text.Unicode.pas',
-  Deltics.IO.Text.Utf8 in '..\src\Deltics.IO.Text.Utf8.pas';
+  Deltics.IO.Text.Utf8 in '..\src\Deltics.IO.Text.Utf8.pas',
+  Test.Utf8Reader in 'Test.Utf8Reader.pas',
+  Test.UnicodeReader in 'Test.UnicodeReader.pas';
 
 begin
   TestRun.Test(Utf8Reader);
